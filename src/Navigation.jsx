ca05b4ae74ae6de82384352847393da4a145f287
mjamecny <jamecny.m@tutanota.com>
2023-10-02 13:12:14 +0200

    add Navigation component

---
+++ src/Navigation.jsx
@@ -0,0 +1,36 @@
+export default function Navigation() {
+  return (
+    <nav className="nav">
+      <ul className="nav__list">
+        <li className="nav__item">
+          <button className="nav__btn nav__btn--add-recipe">
+            <svg className="nav__icon">
+              <use href="icons.svg#icon-edit"></use>
+            </svg>
+            <span>Add recipe</span>
+          </button>
+        </li>
+        <li className="nav__item">
+          <button className="nav__btn nav__btn--bookmarks">
+            <svg className="nav__icon">
+              <use href="icons.svg#icon-bookmark"></use>
+            </svg>
+            <span>Bookmarks</span>
+          </button>
+          <div className="bookmarks">
+            <ul className="bookmarks__list">
+              <div className="message">
+                <div>
+                  <svg>
+                    <use href="icons.svg#icon-smile"></use>
+                  </svg>
+                </div>
+                <p>No bookmarks yet. Find a nice recipe and bookmark it :)</p>
+              </div>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  )
+}
