d8d1c0a9ad092d2c589354ea41f677f34de9bbde
mjamecny <jamecny.m@tutanota.com>
2023-10-02 19:03:38 +0200

    add logic for show bookmarks

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,4 +1,12 @@
+import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
+
+import { getBookmarks, getRecipe } from "./features/recipe/recipeSlice"
+
 export default function Navigation() {
+  const bookmarks = useSelector(getBookmarks)
+  const recipe = useSelector(getRecipe)
+
   return (
     <nav className="nav">
       <ul className="nav__list">
@@ -19,14 +27,47 @@ export default function Navigation() {
           </button>
           <div className="bookmarks">
             <ul className="bookmarks__list">
-              <div className="message">
-                <div>
-                  <svg>
-                    <use href="icons.svg#icon-smile"></use>
-                  </svg>
+              {bookmarks.length === 0 ? (
+                <div className="message">
+                  <div>
+                    <svg>
+                      <use href="icons.svg#icon-smile"></use>
+                    </svg>
+                  </div>
+                  <p>No bookmarks yet. Find a nice recipe and bookmark it :)</p>
                 </div>
-                <p>No bookmarks yet. Find a nice recipe and bookmark it :)</p>
-              </div>
+              ) : (
+                bookmarks.map((bookmark) => {
+                  const { id, publisher, title, image_url } = bookmark
+                  return (
+                    <li key={id} className="preview">
+                      <Link
+                        className={`preview__link ${
+                          id === recipe.id ? "preview__link--active" : ""
+                        }`}
+                        to={`/${id}`}
+                      >
+                        <figure className="preview__fig">
+                          <img src={image_url} alt={title} />
+                        </figure>
+                        <div className="preview__data">
+                          <h4 className="preview__title">{title}</h4>
+                          <p className="preview__publisher">{publisher}</p>
+                          <div
+                            className={`preview__user-generated ${
+                              bookmark.hasOwnProperty("key") ? "" : "hidden"
+                            }`}
+                          >
+                            <svg>
+                              <use href="icons.svg#icon-user"></use>
+                            </svg>
+                          </div>
+                        </div>
+                      </Link>
+                    </li>
+                  )
+                })
+              )}
             </ul>
           </div>
         </li>
