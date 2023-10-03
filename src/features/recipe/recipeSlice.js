8c6010ab6d14b87e09e9b69785c99e3c9eb03fc9
mjamecny <jamecny.m@tutanota.com>
2023-10-03 15:19:45 +0200

    add thunks fot fetch recipes and recipe

--- src/features/recipe/recipeSlice.js
+++ src/features/recipe/recipeSlice.js
@@ -1,23 +1,43 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+
+export const fetchRecipes = createAsyncThunk(
+  "recipe/fetchRecipes",
+  async function (query) {
+    const res = await fetch(
+      `${import.meta.env.VITE_API_URL}?search=${query}&key=${
+        import.meta.env.VITE_KEY
+      }`
+    )
+    const data = await res.json()
+    return data.data.recipes
+  }
+)
+
+export const fetchRecipe = createAsyncThunk(
+  "recipe/fetchRecipe",
+  async function (id) {
+    const res = await fetch(
+      `${import.meta.env.VITE_API_URL}${id}?key=${import.meta.env.VITE_KEY}`
+    )
+    const data = await res.json()
+    return data.data.recipe
+  }
+)
 
 const initialState = {
   recipes: [],
   recipe: {},
   bookmarks: [],
-  isLoading: false,
   showModal: false,
+  statusRecipes: "idle",
+  statusRecipe: "idle",
+  error: "",
 }
 
 const recipeSlice = createSlice({
   name: "recipe",
   initialState,
   reducers: {
-    setRecipes(state, action) {
-      state.recipes = action.payload
-    },
-    setRecipe(state, action) {
-      state.recipe = action.payload
-    },
     addBookmark(state, action) {
       state.bookmarks.push(action.payload)
     },
@@ -26,13 +46,36 @@ const recipeSlice = createSlice({
         (bookmark) => bookmark.id !== action.payload
       )
     },
-    setIsLoading(state, action) {
-      state.isLoading = action.payload
-    },
     setShowModal(state, action) {
       state.showModal = action.payload
     },
   },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchRecipes.pending, (state) => {
+        state.statusRecipes = "loading"
+      })
+      .addCase(fetchRecipes.fulfilled, (state, action) => {
+        state.recipes = action.payload
+        state.statusRecipes = "idle"
+      })
+      .addCase(fetchRecipes.rejected, (state) => {
+        state.statusRecipes = "error"
+        state.error =
+          "There was a problem fetching the recipes. Please try again later."
+      })
+      .addCase(fetchRecipe.pending, (state) => {
+        state.statusRecipe = "loading"
+      })
+      .addCase(fetchRecipe.fulfilled, (state, action) => {
+        state.recipe = action.payload
+        state.statusRecipe = "idle"
+      })
+      .addCase(fetchRecipe.rejected, (state) => {
+        state.statusRecipe = "error"
+        state.error =
+          "There was a problem fetching the recipe. Please try again later."
+      }),
 })
 
 export const getRecipes = (state) => state.recipe.recipes
@@ -43,12 +86,5 @@ export const getBookmarks = (state) => state.recipe.bookmarks
 export const isBookmarked = (state, id) =>
   state.recipe.bookmarks.some((bookmark) => bookmark.id === id)
 
-export const {
-  setRecipes,
-  setRecipe,
-  setIsLoading,
-  setShowModal,
-  addBookmark,
-  removeBookmark,
-} = recipeSlice.actions
+export const { setShowModal, addBookmark, removeBookmark } = recipeSlice.actions
 export default recipeSlice.reducer
