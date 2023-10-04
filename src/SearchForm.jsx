bccb1da492b717d60c42fa94d263b8ed49ee765f
mjamecny <jamecny.m@tutanota.com>
2023-10-04 20:07:44 +0200

    add key shorcut to start searching

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useDispatch } from "react-redux"
 
 import { fetchRecipes, setCurrentPage } from "./features/recipe/recipeSlice"
@@ -6,6 +6,7 @@ import { fetchRecipes, setCurrentPage } from "./features/recipe/recipeSlice"
 export default function SearchForm() {
   const [query, setQuery] = useState("")
   const dispatch = useDispatch()
+  const inputRef = useRef(null)
 
   function handleSubmit(event) {
     event.preventDefault()
@@ -15,14 +16,30 @@ export default function SearchForm() {
     dispatch(setCurrentPage(1))
   }
 
+  useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.key === "/") {
+        event.preventDefault()
+        inputRef.current.focus()
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyDown)
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown)
+    }
+  }, [])
+
   return (
     <form className="search" onSubmit={handleSubmit}>
       <input
         type="text"
         className="search__field"
-        placeholder="Search over 1,000,000 recipes..."
+        placeholder="Click on / to start searching recipes"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        ref={inputRef}
       />
       <button className="btn search__btn">
         <svg className="search__icon">
